97335ad63c28bffb2637ca043c6c4af0cf35ff13
Zakiah07 <zakiah_bloom@yahoo.com>
2022-09-28 02:04:04 +0800

    ts-fundamentals

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 var message = "Welcome Back";
 console.log(message);
@@ -8,42 +23,71 @@ var sum;
 var title = "Codevolution";
 var isBeginner = true;
 var total = 0;
-var name = 'Zakiah';
+var name = "Zakiah";
 var sentence = "My name is ".concat(name, "\nI am a beginner in TypeScript;");
 console.log(sentence);
 var n = null;
 var u = undefined;
-var isNew = null;
-var myName = undefined;
+// let isNew: boolean = null;
+// let myName: string = undefined;
 var list1 = [1, 2, 3];
 var list2 = [1, 2, 3];
-var person1 = ['Chris', 22];
+var person1 = ["Chris", 22];
 var Color;
 (function (Color) {
     Color[Color["Red"] = 5] = "Red";
     Color[Color["Green"] = 6] = "Green";
     Color[Color["Blue"] = 7] = "Blue";
 })(Color || (Color = {}));
-;
-var c = Color.Green;
-console.log(c);
-var randomValue = 10;
-randomValue = true;
-randomValue = "Zakiah";
-var myVariable = 10;
-function hasName(obj) {
-    return !!obj &&
-        typeof obj === "object" &&
-        "name" in obj;
-}
-if (hasName(myVariable)) {
-    console.log(myVariable.name);
-}
-myVariable.toUpperCase();
-var a;
-a = 10;
-a = true;
-var b = 20;
-var multiType;
-multiType = 20;
-multiType = true;
+// let c: Color = Color.Green;
+// console.log(c);
+// let randomValue: unknown = 10;
+// randomValue = true;
+// randomValue = "Zakiah";
+// let myVariable: any = 10;
+// function hasName(obj: any): obj is { name: string } {
+//   return !!obj && typeof obj === "object" && "name" in obj;
+// }
+// if (hasName(myVariable)) {
+//   console.log(myVariable.name);
+// }
+// (myVariable as string).toUpperCase();
+// let a;
+// a = 10;
+// a = true;
+// let b = 20;
+// let multiType: number | boolean;
+// multiType = 20;
+// multiType = true;
+// function add(num1: number, num2: number = 10): number {
+//   if (num2) return num1 + num2;
+//   else return num1;
+// }
+// add(5, 10);
+// add(5);
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good Morning ".concat(this.employeeName));
+    };
+    return Employee;
+}());
+var emp1 = new Employee('Zakiah');
+console.log(emp1.employeeName);
+emp1.greet();
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("Manager delegating tasks");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager('Bruce');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
--- main.ts
+++ main.ts
@@ -1,63 +1,102 @@
-export{}
-let message = "Welcome Back"
+export {};
+let message = "Welcome Back";
 console.log(message);
 
 let x = 10;
-const y = 20
+const y = 20;
 
 let sum;
-const title ="Codevolution";
+const title = "Codevolution";
 
-let isBeginner:boolean = true;
+let isBeginner: boolean = true;
 let total: number = 0;
-let name: string = 'Zakiah';
+let name: string = "Zakiah";
 
 let sentence: string = `My name is ${name}
-I am a beginner in TypeScript;`
+I am a beginner in TypeScript;`;
 
 console.log(sentence);
 
 let n: null = null;
 let u: undefined = undefined;
 
-let isNew: boolean = null;
-let myName: string = undefined
+// let isNew: boolean = null;
+// let myName: string = undefined;
 
-let list1: number[] = [1,2,3];
-let list2: Array<number> = [1, 2, 3]
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
 
-let person1: [string, number] = ['Chris', 22]
+let person1: [string, number] = ["Chris", 22];
 
 enum Color {
-    Red = 5, Green, Blue
-};
-
-let c: Color = Color.Green;
-console.log(c);
-
-let randomValue: unknown = 10;
-randomValue = true;
-randomValue = "Zakiah"
-
-let myVariable: any = 10;
-
-function hasName(obj: any): obj is {name: string} {
-    return !!obj &&
-        typeof obj === "object" &&
-        "name" in obj
+  Red = 5,
+  Green,
+  Blue,
 }
 
-if (hasName(myVariable)) {
-console.log(myVariable.name);
+// let c: Color = Color.Green;
+// console.log(c);
+
+// let randomValue: unknown = 10;
+// randomValue = true;
+// randomValue = "Zakiah";
+
+// let myVariable: any = 10;
+
+// function hasName(obj: any): obj is { name: string } {
+//   return !!obj && typeof obj === "object" && "name" in obj;
+// }
+
+// if (hasName(myVariable)) {
+//   console.log(myVariable.name);
+// }
+// (myVariable as string).toUpperCase();
+
+// let a;
+// a = 10;
+// a = true;
+
+// let b = 20;
+
+// let multiType: number | boolean;
+// multiType = 20;
+// multiType = true;
+
+// function add(num1: number, num2: number = 10): number {
+//   if (num2) return num1 + num2;
+//   else return num1;
+// }
+// add(5, 10);
+// add(5);
+
+
+
+class Employee {
+    public employeeName: string;
+
+    constructor(name: string) {
+        this.employeeName = name;
+    }
+
+    greet() {
+        console.log(`Good Morning ${this.employeeName}`);
+    }
 }
-(myVariable as string).toUpperCase();
 
-let a;
-a=10;
-a=true;
+let emp1 = new Employee('Zakiah')
+console.log(emp1.employeeName);
+emp1.greet();
 
-let b = 20;
+class Manager extends Employee{
+    constructor(managerName: string){
+        super(managerName);
+    }
+    delegateWork() {
+        console.log(`Manager delegating tasks`)
+    }
+}
 
-let multiType: number | boolean;
-multiType = 20;
-multiType = true;
+let m1 = new Manager('Bruce');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName)
